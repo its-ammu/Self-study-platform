f2742b3d51e602c490c159b228c1af423333fc3e
Amutha varsni R <amuthavarsni.cs18@bitsathy.ac.in>
2021-04-29 22:05:55 +0530

    Basics done

---
+++ server.js
@@ -0,0 +1,143 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+app.get('/video1', (req, res) => {
+    const range = req.headers.range;
+    const videoPath = './GraphQL.mp4';
+    const videoSize = fs.statSync(videoPath).size;
+
+    const chunkSize = 1 * 1e+6;
+    const start = Number(range.replace(/\D/g, ''));
+    const end = Math.min(start + chunkSize, videoSize -1);
+
+    const contentLength = end - start + 1;
+
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4"
+    }
+    res.writeHead(206, headers);
+
+    const stream = fs.createReadStream(videoPath, { start, end })
+    stream.pipe(res);
+});
+
+app.get('/video2', (req, res) => {
+    const range = req.headers.range;
+    const videoPath = './GraphQL.mp4';
+    const videoSize = fs.statSync(videoPath).size;
+
+    const chunkSize = 1 * 1e+6;
+    const start = Number(range.replace(/\D/g, ''));
+    const end = Math.min(start + chunkSize, videoSize -1);
+
+    const contentLength = end - start + 1;
+
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4"
+    }
+    res.writeHead(206, headers);
+
+    const stream = fs.createReadStream(videoPath, { start, end })
+    stream.pipe(res);
+});
+
+app.get('/video3', (req, res) => {
+    const range = req.headers.range;
+    const videoPath = './GraphQL.mp4';
+    const videoSize = fs.statSync(videoPath).size;
+
+    const chunkSize = 1 * 1e+6;
+    const start = Number(range.replace(/\D/g, ''));
+    const end = Math.min(start + chunkSize, videoSize -1);
+
+    const contentLength = end - start + 1;
+
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4"
+    }
+    res.writeHead(206, headers);
+
+    const stream = fs.createReadStream(videoPath, { start, end })
+    stream.pipe(res);
+});
+
+app.get('/video4', (req, res) => {
+    const range = req.headers.range;
+    const videoPath = './GraphQL.mp4';
+    const videoSize = fs.statSync(videoPath).size;
+
+    const chunkSize = 1 * 1e+6;
+    const start = Number(range.replace(/\D/g, ''));
+    const end = Math.min(start + chunkSize, videoSize -1);
+
+    const contentLength = end - start + 1;
+
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4"
+    }
+    res.writeHead(206, headers);
+
+    const stream = fs.createReadStream(videoPath, { start, end })
+    stream.pipe(res);
+});
+
+app.get('/video5', (req, res) => {
+    const range = req.headers.range;
+    const videoPath = './GraphQL.mp4';
+    const videoSize = fs.statSync(videoPath).size;
+
+    const chunkSize = 1 * 1e+6;
+    const start = Number(range.replace(/\D/g, ''));
+    const end = Math.min(start + chunkSize, videoSize -1);
+
+    const contentLength = end - start + 1;
+
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4"
+    }
+    res.writeHead(206, headers);
+
+    const stream = fs.createReadStream(videoPath, { start, end })
+    stream.pipe(res);
+});
+
+app.get('/video6', (req, res) => {
+    const range = req.headers.range;
+    const videoPath = './GraphQL.mp4';
+    const videoSize = fs.statSync(videoPath).size;
+
+    const chunkSize = 1 * 1e+6;
+    const start = Number(range.replace(/\D/g, ''));
+    const end = Math.min(start + chunkSize, videoSize -1);
+
+    const contentLength = end - start + 1;
+
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4"
+    }
+    res.writeHead(206, headers);
+
+    const stream = fs.createReadStream(videoPath, { start, end })
+    stream.pipe(res);
+});
+app.listen('3000');
